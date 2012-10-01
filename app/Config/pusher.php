04abe2cc9e3073dabb1439d8dddd21bf7c2292cf
maehira <y.maehira@gmail.com>
2012-10-01 23:33:24 +0900

    mock

--- app/Config/pusher.php
+++ app/Config/pusher.php
@@ -1,7 +1,7 @@
 <?php
 
 $config["Pusher"] = array(
-    "key"  => "9edee0e1a1fb10db4b6e",
-    "secret" => "e1c7ba2f7000b6ee8d5e",
-    "app_id"   => "28675"
+    "key"  => "********",
+    "secret" => "********",
+    "app_id"   => "*****"
 );
